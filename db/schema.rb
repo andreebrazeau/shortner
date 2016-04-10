b7b351437e49ab66485e051064b10cea5eb27c77
Andrée Brazeau <abrazeau@bitcasa.com>
2016-04-10 09:22:41 -0700

    configure to run on Postgres

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20160406033756) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "mappings", force: :cascade do |t|
     t.string   "url_hash"
     t.text     "long_url"
